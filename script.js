5630ddb8718c6eb7f0e5fc0fef615494b41b8271
Suresh Kumar <ft24-bscs-0022@qu.edu.pk>
2024-11-30 23:40:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Get elements by tag name
+var a = document.getElementsByTagName('li');
+console.log(a);
+
+// Get elements by class name
+var b = document.getElementsByClassName('hero');
+console.log(b);
+
+// Get element by ID
+var c = document.getElementById('hero');
+console.log(c);
+
+// Query selector: Select one element by ID
+var d = document.querySelector('#hero');
+console.log(d);
+
+// Query selector: Select one element by class
+var d = document.querySelector('.hero');
+console.log(d);
+
+// Query selector all: Select all elements by class
+var e = document.querySelectorAll('.hero');
+console.log(e);
